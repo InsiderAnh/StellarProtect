1bcf49bade0033541b8c4e427118773e84d0a1eb
InsiderAnh <leo_yt_29@hotmail.com>
2025-08-11 23:17:30 -0500

    ItemTemplate and ItemTemplateLight to remove shorted property and simplify base64 handling

--- API/src/main/java/io/github/insideranh/stellarprotect/items/ItemTemplate.java
+++ API/src/main/java/io/github/insideranh/stellarprotect/items/ItemTemplate.java
@@ -1,6 +1,5 @@
 package io.github.insideranh.stellarprotect.items;
 
-import io.github.insideranh.stellarprotect.utils.StringCleanerUtils;
 import lombok.Getter;
 import org.bukkit.inventory.ItemStack;
 
@@ -10,18 +9,11 @@ public class ItemTemplate {
     private final long id;
     private final ItemStack bukkitItem;
     private final String base64;
-    // 0 = false | 1 = true
-    private byte shorted = 0;
 
     public ItemTemplate(long id, ItemStack bukkitItem, String base64) {
         this.id = id;
         this.bukkitItem = bukkitItem;
-        if (base64.startsWith(StringCleanerUtils.COMMON_BASE64)) {
-            this.base64 = base64.substring(StringCleanerUtils.COMMON_BASE64.length());
-            this.shorted = 1;
-        } else {
-            this.base64 = base64;
-        }
+        this.base64 = base64;
     }
 
 }
--- API/src/main/java/io/github/insideranh/stellarprotect/items/memory/ItemTemplateLight.java
+++ API/src/main/java/io/github/insideranh/stellarprotect/items/memory/ItemTemplateLight.java
@@ -1,6 +1,5 @@
 package io.github.insideranh.stellarprotect.items.memory;
 
-import io.github.insideranh.stellarprotect.utils.StringCleanerUtils;
 import lombok.Getter;
 
 @Getter
@@ -8,17 +7,10 @@ public class ItemTemplateLight {
 
     private final long id;
     private final String base64;
-    // 0 = false | 1 = true
-    private byte shorted = 0;
 
     public ItemTemplateLight(long id, String base64) {
         this.id = id;
-        if (base64.startsWith(StringCleanerUtils.COMMON_BASE64)) {
-            this.base64 = base64.substring(StringCleanerUtils.COMMON_BASE64.length());
-            this.shorted = 1;
-        } else {
-            this.base64 = base64;
-        }
+        this.base64 = base64;
     }
 
 }
