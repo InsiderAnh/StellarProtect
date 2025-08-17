f09febedca8e70a40f5dc0cce456eded26d7464b
InsiderAnh <leo_yt_29@hotmail.com>
2025-08-17 10:04:41 -0500

    perf: optimized teleporttype enum

--- Common/src/main/java/io/github/insideranh/stellarprotect/enums/TeleportType.java
+++ Common/src/main/java/io/github/insideranh/stellarprotect/enums/TeleportType.java
@@ -2,6 +2,9 @@ package io.github.insideranh.stellarprotect.enums;
 
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 public enum TeleportType {
 
@@ -19,26 +22,103 @@ public enum TeleportType {
 
     private final int id;
 
+    private static final TeleportType[] ID_CACHE = new TeleportType[11];
+    private static final Map<String, TeleportType> NAME_CACHE;
+
+    static {
+        TeleportType[] values = values();
+
+        for (TeleportType type : values) {
+            ID_CACHE[type.id] = type;
+        }
+
+        NAME_CACHE = new HashMap<>(values.length * 2);
+
+        for (TeleportType type : values) {
+            String name = type.name();
+            NAME_CACHE.put(name, type);
+            NAME_CACHE.put(name.toLowerCase(), type);
+
+            if (name.contains("_")) {
+                String spaceName = name.replace("_", " ");
+                NAME_CACHE.put(spaceName, type);
+                NAME_CACHE.put(spaceName.toLowerCase(), type);
+            }
+        }
+    }
+
     TeleportType(int id) {
         this.id = id;
     }
 
     public static TeleportType getByName(String name) {
-        for (TeleportType teleportType : TeleportType.values()) {
-            if (teleportType.name().equalsIgnoreCase(name)) {
-                return teleportType;
+        if (name == null) {
+            return UNKNOWN;
+        }
+
+        TeleportType cached = NAME_CACHE.get(name);
+        if (cached != null) {
+            return cached;
+        }
+
+        cached = NAME_CACHE.get(name.toLowerCase());
+        if (cached != null) {
+            return cached;
+        }
+
+        int len = name.length();
+        TeleportType[] values = values();
+
+        for (TeleportType type : values) {
+            String typeName = type.name();
+            if (typeName.length() != len) {
+                continue;
+            }
+
+            if (equalsIgnoreCase(name, typeName)) {
+                return type;
             }
         }
-        return TeleportType.UNKNOWN;
+
+        return UNKNOWN;
     }
 
     public static TeleportType getById(int id) {
-        for (TeleportType teleportType : TeleportType.values()) {
-            if (teleportType.getId() == id) {
-                return teleportType;
+        if (id >= 0 && id < ID_CACHE.length) {
+            return ID_CACHE[id];
+        }
+        return UNKNOWN;
+    }
+
+    public static TeleportType getByOrdinal(int ordinal) {
+        TeleportType[] values = values();
+        if (ordinal >= 0 && ordinal < values.length) {
+            return values[ordinal];
+        }
+        return UNKNOWN;
+    }
+
+    private static boolean equalsIgnoreCase(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
+
+            if (c1 >= 'A' && c1 <= 'Z') {
+                c1 = (char) (c1 | 0x20);
+            }
+            if (c2 >= 'A' && c2 <= 'Z') {
+                c2 = (char) (c2 | 0x20);
+            }
+
+            if (c1 != c2) {
+                return false;
             }
         }
-        return TeleportType.UNKNOWN;
+        return true;
     }
 
 }
