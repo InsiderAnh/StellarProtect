b35de04e20b630327a8c219c91747b26f22e67f4
InsiderAnh <leo_yt_29@hotmail.com>
2025-09-11 09:22:41 -0500

    feat: added inspect break block

--- Common/src/main/java/io/github/insideranh/stellarprotect/listeners/InspectListener.java
+++ Common/src/main/java/io/github/insideranh/stellarprotect/listeners/InspectListener.java
@@ -5,12 +5,15 @@ import io.github.insideranh.stellarprotect.data.InspectSession;
 import io.github.insideranh.stellarprotect.data.PlayerProtect;
 import io.github.insideranh.stellarprotect.utils.WorldUtils;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class InspectListener implements Listener {
@@ -22,26 +25,36 @@ public class InspectListener implements Listener {
         Player player = event.getPlayer();
         if (event.getClickedBlock() == null) return;
 
+        Action action = event.getAction();
+
+        if (!action.equals(Action.LEFT_CLICK_BLOCK) && !action.equals(Action.RIGHT_CLICK_BLOCK))
+            return;
+
         PlayerProtect playerProtect = PlayerProtect.getPlayer(player);
-        if (playerProtect == null || !playerProtect.isInspect())
-            return;
-        event.setCancelled(true);
+        if (cantInspect(playerProtect, event)) return;
 
-        if (!event.getAction().equals(Action.LEFT_CLICK_BLOCK) && !event.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-            return;
+        Block block = event.getClickedBlock();
+        Location blockLocation = block.getLocation();
+        playerProtect.setInspectSession(new InspectSession(blockLocation, 0, 10, WorldUtils.isValidChestBlock(block.getType())));
 
-        if (playerProtect.getNextInspect() > System.currentTimeMillis())
-            return;
-        playerProtect.setNextInspect(System.currentTimeMillis() + 500L);
-
-        Location blockLocation = event.getClickedBlock().getLocation();
-        playerProtect.setInspectSession(new InspectSession(blockLocation, 0, 10, WorldUtils.isValidChestBlock(event.getClickedBlock().getType())));
-
-        if (WorldUtils.isValidChestBlock(event.getClickedBlock().getType()) && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+        if (WorldUtils.isValidChestBlock(block.getType()) && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             plugin.getInspectHandler().handleChestInspection(player, blockLocation, 1, 0, 10);
         } else {
             plugin.getInspectHandler().handleBlockInspection(player, blockLocation, 1, 0, 10);
         }
+    }
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        PlayerProtect playerProtect = PlayerProtect.getPlayer(player);
+        if (cantInspect(playerProtect, event)) return;
+
+        Block block = event.getBlockPlaced();
+        Location blockLocation = block.getLocation();
+        playerProtect.setInspectSession(new InspectSession(blockLocation, 0, 10, WorldUtils.isValidChestBlock(block.getType())));
+
+        plugin.getInspectHandler().handleBlockInspection(player, blockLocation, 1, 0, 10);
     }
 
     @EventHandler
@@ -52,6 +65,18 @@ public class InspectListener implements Listener {
             return;
 
         event.setCancelled(true);
+    }
+
+    boolean cantInspect(PlayerProtect playerProtect, Cancellable event) {
+        if (playerProtect == null || !playerProtect.isInspect())
+            return true;
+        event.setCancelled(true);
+
+        if (playerProtect.getNextInspect() > System.currentTimeMillis())
+            return true;
+        playerProtect.setNextInspect(System.currentTimeMillis() + 500L);
+
+        return false;
     }
 
 }
