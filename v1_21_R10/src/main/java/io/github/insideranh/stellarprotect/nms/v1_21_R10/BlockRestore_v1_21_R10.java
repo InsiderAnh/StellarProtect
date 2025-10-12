7f0063fe22bc1e9f46432dc25d1dab907168c9cd
InsiderAnh <leo_yt_29@hotmail.com>
2025-10-12 16:49:15 -0500

    feat: added support 1.21.10

---
+++ v1_21_R10/src/main/java/io/github/insideranh/stellarprotect/nms/v1_21_R10/BlockRestore_v1_21_R10.java
@@ -0,0 +1,37 @@
+package io.github.insideranh.stellarprotect.nms.v1_21_R10;
+
+import com.google.gson.Gson;
+import io.github.insideranh.stellarprotect.restore.BlockRestore;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Player;
+
+public class BlockRestore_v1_21_R10 extends BlockRestore {
+
+    public BlockRestore_v1_21_R10(String data) {
+        super(data);
+    }
+
+    @Override
+    public void reset(Gson gson, Location location) {
+        BlockData blockData = Bukkit.createBlockData(getData());
+
+        Block block = location.getBlock();
+        block.setBlockData(blockData);
+    }
+
+    @Override
+    public void preview(Player player, Gson gson, Location location) {
+        BlockData blockData = Bukkit.createBlockData(getData());
+        player.sendBlockChange(location, blockData);
+    }
+
+    @Override
+    public void previewRemove(Player player, Location location) {
+        player.sendBlockChange(location, Material.AIR.createBlockData());
+    }
+
+}
---
+++ v1_21_R10/src/main/java/io/github/insideranh/stellarprotect/nms/v1_21_R10/DataBlock_v1_21_R10.java
@@ -0,0 +1,61 @@
+package io.github.insideranh.stellarprotect.nms.v1_21_R10;
+
+import io.github.insideranh.stellarprotect.blocks.DataBlock;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Levelled;
+
+public class DataBlock_v1_21_R10 implements DataBlock {
+
+    @Getter
+    private final String blockDataString;
+    private BlockData blockData;
+
+    public DataBlock_v1_21_R10(String blockDataString) {
+        this.blockData = Bukkit.createBlockData(blockDataString);
+        this.blockDataString = blockDataString;
+    }
+
+    public DataBlock_v1_21_R10(Block block) {
+        this.blockData = block.getBlockData();
+        if (blockData instanceof Levelled) {
+            Levelled levelled = (Levelled) blockData;
+            int currentLevel = levelled.getLevel();
+            int maxLevel = levelled.getMaximumLevel();
+            int newLevel = (currentLevel + 1) % (maxLevel + 1);
+            levelled.setLevel(newLevel);
+            this.blockData = levelled;
+        }
+        this.blockDataString = blockData.getAsString();
+    }
+
+    public DataBlock_v1_21_R10(BlockState block) {
+        this.blockData = block.getBlockData();
+        if (blockData instanceof Levelled) {
+            Levelled levelled = (Levelled) blockData;
+            int currentLevel = levelled.getLevel();
+            int maxLevel = levelled.getMaximumLevel();
+            int newLevel = (currentLevel + 1) % (maxLevel + 1);
+            levelled.setLevel(newLevel);
+            this.blockData = levelled;
+        }
+        this.blockDataString = blockData.getAsString();
+    }
+
+    @Override
+    public int hashCode() {
+        return blockData.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof DataBlock_v1_21_R10) {
+            return blockData.equals(((DataBlock_v1_21_R10) obj).blockData);
+        }
+        return false;
+    }
+
+}
